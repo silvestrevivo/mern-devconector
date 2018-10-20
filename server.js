04f684207048126c1e590d87a6d5b753c6630112
silvestrevivo <silvestrevivomillan@gmail.com>
2018-10-20 23:13:22 +0200

    connecting to data base with mlab

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 'use strict'
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.get('/', (req, res) => res.send('hello'))
+app.set('port', process.env.PORT || 5000)
 
-const port = process.env.PORT || 5000
+// DB config
+const db = require('./config/keys').mongoURI
+mongoose.Promise = global.Promise
+mongoose.set('useCreateIndex', true)
+mongoose.connect(
+  db,
+  { useNewUrlParser: true },
+)
 
-app.listen(port, () => console.log(`Listening running on port ${port}`))
+mongoose.connection
+  .once('open', () => {
+    // making the app listening to port
+    app.listen(app.get('port'), () => {
+      console.log(`Server started on port ${app.get('port')}`)
+    })
+  })
+  .on('error', error => {
+    return console.warn(`Error connecting to the data base: ${error}`)
+  })
