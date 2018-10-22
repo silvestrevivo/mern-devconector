4260858621e1decd0ea1c4565609598a96652ad8
silvestrevivo <silvestrevivomillan@gmail.com>
2018-10-22 14:01:15 +0200

    layout home, register and login

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  onChange = e => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    const { email, password } = this.state
+    const user = { email, password }
+    console.log('user', user)
+  }
+
+  render() {
+    const { email, password } = this.state
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login
