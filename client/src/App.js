621e1f6f7ab575f1c2f9adbd525fc6d9afdf3e52
silvestrevivo <silvestrevivomillan@gmail.com>
2018-10-26 14:37:51 +0200

    protected routes done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
 import { setCurrentUser, logoutUser } from './actions/authActions'
+
 import { Provider } from 'react-redux'
 import store from './store'
+
+import PrivateRoute from './components/common/PrivateRoute'
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import Dashboard from './components/dashboard/Dashboard'
 
 import './App.css'
 
@@ -46,6 +50,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
